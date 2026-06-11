0be93cdf744be10d514632e2f989f234cdc4fed1
PUYadav <PUYadav@OPT-LAP-0068>
2026-06-11 21:43:10 +0530

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './Components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home, title: 'Home'},
+    {path: 'home', component: Home, title: 'Home'},
+];
